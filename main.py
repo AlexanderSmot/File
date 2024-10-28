7c91d309edf403ded92403c5f5e02bfc8c76a26c
Alex <virus2005@yandex.ru>
2024-10-28 15:05:20 +0400

    changed main

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 #1 Задача
 
 with open('recipes.txt') as file:
@@ -47,35 +49,11 @@ for t in mlist:
     count = 0
 print(count_list)
 print(sorted(count_list))
+print(f_list)
 
 
-with open('main.txt', 'w') as f:
-    f.write('1.txt'.read())
+# file_path = os.path.join(os.getcwd(), '1.txt')
+# print(file_path)
+# with open('main.txt', 'w') as f:
+#     f.write('1.txt'.read())
 
-
-
-
-
-
-
-# def rewriting(file_for_writing: str, base_path, location):
-#     files = []
-#     for i in list(os.listdir(os.path.join(base_path, location))):
-#         files.append([acounting(os.path.join(base_path, location, i)), os.path.join(base_path, location, i), i])
-#     for file_from_list in sorted(files):
-#         opening_files = open(file_for_writing, 'a')
-#         opening_files.write(f'{file_from_list[2]}\n')
-#         opening_files.write(f'{file_from_list[0]}\n')
-#         with open(file_from_list[1], 'r',encoding='utf-8') as file:
-#             counting = 1
-#             for line in file:
-#                 opening_files.write(f'строка № {counting} в файле {file_from_list[2]} : {line}')
-#                 counting += 1
-#         opening_files.write(f'\n')
-#         opening_files.close()
-#
-#
-# file_for_writing = os.path.abspath('\\Users\\йролджэ\\Desktop\\Python\\1.txt')
-# base_path = os.getcwd()
-# location = os.path.abspath('\\Users\\йролджэ\\Desktop\\Python')
-# rewriting(file_for_writing, base_path, location)
